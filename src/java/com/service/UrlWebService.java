8c9d126a560038e1121d01cbbbeb1feae904158b
Daniel Tome <daniel.tome@estudiants.urv.cat>
2014-12-31 15:40:37 +0100

    Añadiendo proyecto servicio web

---
+++ src/java/com/service/UrlWebService.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.service;
+
+import javax.annotation.Resource;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author Daniel Tomé <daniel.tome@estudiants.urv.cat>
+ */
+@WebService(serviceName = "UrlWebService")
+public class UrlWebService {
+    @Resource(name = "SOB_P1_DB")
+    private DataSource SOB_P1_DB;
+
+    /**
+     * This is a sample web service operation
+     * @param txt
+     * @return 
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
